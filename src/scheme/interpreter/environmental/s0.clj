d36bd185b02293487223f5c9e7c227252d3a7c9f
Piotr Zielinski <zielinski_piotr@hotmail.com>
2012-09-24 22:00:11 -0400

    s0 is now s;

---
+++ src/scheme/interpreter/environmental/s0.clj
@@ -0,0 +1,481 @@
+(ns scheme.interpreter.environmental.s0)
+
+(use 'clojure.test)
+;(use 'clojure.test.junit)
+
+(use 'scheme.interpreter.utils)
+(use 'scheme.interpreter.syntax)
+(use 'scheme.interpreter.environmental.env)
+
+;clojure specific
+(def apply-in-underlying-interpreter 
+  apply)
+
+(def global-primitive-procedure-impl-map
+  {
+   '* *
+   '+ +
+   '- -
+   '> >
+   '< <
+   '= =
+   '>= >=
+   '<= <=
+   '== ==
+   'car first
+   'cdr rest
+   'cons cons
+   'null? nil?
+   }
+)
+
+(declare analyze)
+(declare do-eval)
+
+(defn make-result
+  [return env]
+    {:return return :env env}
+  )
+
+(defn get-result-return
+  [eval-result]
+    (:return eval-result)
+  )
+
+(defn get-result-env
+  [eval-result]
+    (:env eval-result)
+  )
+
+(defn make-identity-eval-fn 
+  [x]
+  (fn [env] (make-result x env))
+  )
+
+(defn primitive-procedure-impl
+  [primitive-procedure-impl-map, k]
+  (primitive-procedure-impl-map k)
+  )
+
+(defn make-begin 
+  [the-sequence] 
+  (cons 'begin the-sequence)
+  )
+
+(defn make-if 
+  [predicate consequent alternative]
+  (list 'if predicate consequent alternative)
+  )
+
+(defn make-primitive 
+  [implementation] 
+  (list 'primitive implementation)
+  )
+
+(defn make-primitives-map 
+  [primitive-procedure-impl-map]
+  (map-the-map identity (fn [x] (make-identity-eval-fn (make-primitive x))) primitive-procedure-impl-map)
+  )
+
+(defn make-procedure 
+  [parameters body env] 
+  (list 'procedure parameters body env))
+
+(defn apply-primitive-procedure 
+  [procedure arguments] 
+  (apply-in-underlying-interpreter (primitive-implementation procedure) arguments))
+
+(defn assignment-variable 
+  [exp] 
+  (nth exp 1))
+
+(defn assignment-value 
+  [exp] 
+  (nth exp 2))
+
+(defn begin-actions 
+  [exp] 
+  (rest exp))
+
+(defn cond-clauses 
+  [exp] 
+  (rest exp))
+
+(defn cond-clause-actions 
+  [clause] 
+  (rest clause))
+
+(defn cond-clause-predicate 
+  [clause] 
+  (first clause))
+
+(defn cond-else-clause? 
+  [clause] 
+  (= (cond-clause-predicate clause) 'else))
+
+(defn sequence->exp 
+  [the-sequence]
+  (cond 
+    (empty? the-sequence) the-sequence
+    (= (count the-sequence) 1) (first the-sequence)
+    :else (make-begin the-sequence)))
+
+(defn expand-clauses [clauses]
+  (if (empty? clauses)
+    'false
+    (if (cond-else-clause? (first clauses))
+      (if (empty? (rest clauses))
+        (sequence->exp (cond-clause-actions (first clauses)))
+        (error "ELSE clause isn't last -- COND->IF" clauses))
+      (make-if 
+        (cond-clause-predicate (first clauses)) 
+        (sequence->exp (cond-clause-actions (first clauses)))
+        (expand-clauses (rest clauses))))))
+
+(defn cond->if 
+  [exp] 
+  (expand-clauses (cond-clauses exp)))
+
+(defn definition-variable 
+  [exp] 
+  (nth exp 1))
+
+(defn definition-value 
+  [exp] 
+  (nth exp 2))
+
+(defn text-of-quotation 
+  [exp] 
+  (nth exp 1))
+
+(defn if-alternative 
+  [exp] 
+  (if (= (count exp) 4) (nth exp 3) 'false))
+
+(defn if-consequent 
+  [exp] 
+  (nth exp 2))
+
+(defn if-predicate 
+  [exp] 
+  (nth exp 1))
+
+(defn lambda-body 
+  [exp] 
+  (nth exp 2))
+
+(defn lambda-parameters 
+  [exp] 
+  (nth exp 1))
+
+(defn operands 
+  [exp] 
+  (rest exp))
+
+(defn operator 
+  [exp] 
+  (first exp))
+
+(defn procedure-body 
+  [exp] 
+  (nth exp 2))
+
+(defn procedure-environment 
+  [exp] 
+  (nth exp 3))
+
+(defn procedure-parameters 
+  [exp] 
+  (nth exp 1))
+
+(defn analyze-self-evaluating 
+  [exp] 
+  (fn [env] 
+    (make-result exp env)
+    )
+  )
+
+(defn analyze-variable 
+  [exp] 
+  (fn [env] 
+    (let [value-proc (lookup-variable-value-in-env exp env)
+          value-proc-result (value-proc env)
+          value-proc-return-value (get-result-return value-proc-result)]
+      (make-result value-proc-return-value env)));keep original env
+  )
+
+(defn analyze-quotation 
+  [exp]
+  (fn [env]
+    (make-result (text-of-quotation exp) env)
+    )
+  )
+
+(defn analyze-assignment 
+  [exp] 
+  (let [variable (assignment-variable exp)
+        value-proc (analyze (assignment-value exp))]
+    (fn [env]
+      (make-result 
+        'ok 
+        (set-variable-value-in-env variable value-proc env));use original env
+      )
+    )
+  )
+
+(defn analyze-cond 
+  [exp]
+  (let [condifexp-proc (analyze (cond->if exp))] 
+    (fn [env]
+      (let [cond-result (condifexp-proc env)
+            cond-return-val (get-result-return cond-result)]
+        (make-result cond-return-val env);use original env
+        )
+      )
+    )
+  )
+
+(defn analyze-definition-of-function 
+  [exp] 
+  (let [variable (definition-variable exp)
+        function-name (first variable)
+        params (rest variable)
+        body-proc (analyze (definition-value exp))];single expression only, use begin!!!
+    (fn [env] 
+      (make-result 
+        'ok
+        ;use new env, created by adding def
+        (set-variable-value-in-env 
+          function-name 
+          (fn [env]
+            (make-result (make-procedure params body-proc env) env)
+            )
+          env
+          )))
+    )
+  )
+
+(defn analyze-definition-of-variable 
+  [exp] 
+  (let [variable (definition-variable exp)
+        value-proc (analyze (definition-value exp))]
+    (fn [env]
+      (make-result 
+        'ok
+        ;use new env, created by adding def
+        (set-variable-value-in-env variable value-proc env))
+      )
+    )
+  )
+
+(defn analyze-definition 
+  [exp] 
+  (let [variable (definition-variable exp)]
+    (if (variable? variable)
+      (analyze-definition-of-variable exp)
+      (analyze-definition-of-function exp))
+    )
+  )
+
+(defn analyze-lambda 
+  [exp] 
+  (let [params (lambda-parameters exp)
+        body-proc (analyze (lambda-body exp));single expression only, use begin!
+        ]
+    (fn [env]
+      (make-result (make-procedure params body-proc env) env)
+      ))
+  )
+
+(defn analyze-if 
+  [exp]
+  (let [predicate-proc (analyze (if-predicate exp))
+        consequent-proc (analyze (if-consequent exp))
+        alternative-proc (analyze (if-alternative exp))]
+    (fn [env]
+      (if (true? (get-result-return (predicate-proc env)))
+        (consequent-proc env)
+        (alternative-proc env))
+      )
+    )
+  )
+
+(defn analyze-sequence 
+  [exps]
+  (cond
+    (empty? exps) 
+    (fn [env] )
+    (= (count exps) 1) 
+    (let [exp (first exps)
+          exp-proc (analyze exp)]
+      (fn [env] 
+        (let [exp-result (exp-proc env) 
+              exp-ret (get-result-return exp-result)
+              exp-env (get-result-env exp-result)]
+          (make-result exp-ret exp-env)))
+      )
+    :else
+    (reduce 
+      (fn [fn1 fn2]
+        (fn [env] 
+          (let [exp1-result (fn1 env) 
+                exp1-ret (get-result-return exp1-result)
+                exp1-env (get-result-env exp1-result)
+                exp2-result (fn2 exp1-env);use env returned by eval of previous exp
+                exp2-ret (get-result-return exp2-result)
+                exp2-env (get-result-env exp2-result)]
+            (make-result exp2-ret exp2-env))))
+      ;create a list of functions fn[env]
+      (map 
+        (fn [exp] 
+          (let [exp-proc (analyze exp)]
+            (fn [env] 
+              (let [exp-result (exp-proc env) 
+                    exp-ret (get-result-return exp-result)
+                    exp-env (get-result-env exp-result)]
+                (make-result exp-ret exp-env)));pass new env
+            )) 
+        exps)
+      )
+    )
+  )
+
+(defn analyze-begin
+  [exp]
+  (analyze-sequence (begin-actions exp)) 
+)
+
+(defn execute-application 
+  [procedure arg-procs] 
+  (cond 
+    (primitive-procedure? procedure)
+    (let [args (map #(get-result-return (% nil)) arg-procs)];nil env, use captured one
+      (apply-primitive-procedure procedure args))
+    (compound-procedure? procedure) 
+       (let [params (procedure-parameters procedure)
+             proc-env (procedure-environment procedure)
+             env (extend-environment params arg-procs proc-env)
+             body (procedure-body procedure)]
+         (get-result-return (body env))
+         )
+    :else 
+       (error "Unknown procedure type -- APPLY" procedure)))
+
+(defn analyze-application
+  [exp] 
+  (let [operator-proc (analyze (operator exp))
+        arg-procs (map #(analyze %) (operands exp))]
+    (fn [env] 
+      (let [operator (get-result-return (operator-proc env))
+            ;capture this env
+            arg-env-procs (map (fn [arg-proc] (fn [proc-env] (arg-proc env))) arg-procs)]
+        (make-result (execute-application operator arg-env-procs) env);keep original env
+        )
+      )
+    )
+  )
+
+(defn setup-environment
+  [primitive-procedure-impl-map env]
+  (let [primitives-map (make-primitives-map primitive-procedure-impl-map)
+        env1 (set-variable-value-in-env 'true (make-identity-eval-fn true) env)
+        env2 (set-variable-value-in-env 'false (make-identity-eval-fn false) env1)]
+    (extend-environment-with-map primitives-map env2)
+    )
+  )
+
+(def global-analyze-map
+  {
+   'quote analyze-quotation
+   'cond analyze-cond
+   'if analyze-if
+   'lambda analyze-lambda
+   'begin analyze-begin
+   }
+)
+
+(defn can-analyze-from-map? 
+  [the-map exp]
+    (if (empty? exp)
+      false
+      (not (nil? (the-map (first exp)))))
+)
+
+(defn define?
+  [exp]
+  (if (empty? exp)
+    false
+    (= 'define (first exp)))
+  )
+
+(defn set!?
+  [exp]
+  (if (empty? exp)
+    false
+    (= 'set! (first exp)))
+  )
+
+(defn do-analyze-from-map 
+  [the-map exp]
+  (let [proc (the-map (first exp))]
+    ;each proc returns fn[env] that returns map
+    (proc exp))
+)
+
+(defn analyze 
+  [exp]
+  (cond 
+    (self-evaluating? exp) (analyze-self-evaluating exp)
+    (variable? exp) (analyze-variable exp)
+    (can-analyze-from-map? global-analyze-map exp) (do-analyze-from-map global-analyze-map exp)
+    (define? exp) (analyze-definition exp)
+    (set!? exp) (analyze-assignment exp)
+    (application? exp) (analyze-application exp)
+    :else (error "Unknown expression type -- EVAL" exp))
+  )
+
+(defn do-eval 
+  [exp env]
+  (let [proc (analyze exp)]
+    (proc env)
+    )
+  )
+
+(defn user-print 
+  [object]
+  (if (compound-procedure? object)
+    (println 
+      (list 
+        'compound-procedure
+        (procedure-parameters object)
+        (procedure-body object)
+        '<procedure-env>))
+    (println object)
+    )
+  )
+
+(def input-prompt "in:")
+
+(def output-prompt "=>")
+
+(defn repl
+  []
+  (loop [env (setup-environment global-primitive-procedure-impl-map (the-empty-environment))]
+    (let [input (read)]
+      (println input)
+      (if (= (str input) "exit")
+        (println "exit requested by user")
+        (if (nil? input)
+          (recur env)
+          (let [output-result (do-eval input env)
+                output-value (get-result-return output-result)
+                output-env (get-result-env output-result)]
+            (print output-prompt)
+            (user-print output-value)
+            (recur output-env)
+            )
+          )
+        )
+      )
+    )
+  )
---
+++ test/scheme/interpreter/environmental/s0_test.clj
@@ -0,0 +1,132 @@
+(ns scheme.interpreter.environmental.s0-test)
+
+(use 'clojure.test)
+;(use 'clojure.test.junit)
+
+(use 'scheme.interpreter.environmental.env)
+(use 'scheme.interpreter.environmental.s0)
+
+(deftest primitive-procedure-impl-test
+  (is (= first (primitive-procedure-impl (hash-map 'car first) 'car)))
+  )
+
+(deftest make-primitive-test 
+  (is (= (list 'primitive cons) (make-primitive cons)))
+  )
+
+(deftest can-analyze-from-map?-test 
+  (is (= true (can-analyze-from-map? global-analyze-map '(quote x))))
+  (is (= false (can-analyze-from-map? global-analyze-map '(x))))
+  (is (= false (can-analyze-from-map? global-analyze-map '())))
+  )
+
+(deftest do-analyze-from-map-test 
+  (is (= 'x (get-result-return ((do-analyze-from-map global-analyze-map '(quote x)) nil))))
+  )
+
+(def factorial
+  (fn [n]
+    (loop [cnt n acc 1]
+       (if (zero? cnt)
+            acc
+          (recur (dec cnt) (* acc cnt))))))
+
+(deftest do-eval-test 
+  (let [env (setup-environment global-primitive-procedure-impl-map (the-empty-environment))]
+    ;primitive
+    (is (= false (get-result-return (do-eval 'false env))))
+    (is (= true (get-result-return (do-eval 'true env))))
+    (is (= (make-primitive +) (get-result-return (do-eval '+ env))))
+    (is (= (make-primitive first) (get-result-return (do-eval 'car env))))
+    ;self-evaluating
+    (is (= 1 (get-result-return (do-eval 1 env))))
+    (is (= "" (get-result-return (do-eval "" env))))
+    (is (= "1" (get-result-return (do-eval "1" env))))
+    ;variable
+    ;(is (thrown? Throwable (do-eval 'v env)) "Variable is not bound!");!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    (is (= 1 (get-result-return (do-eval 'v (extend-environment-with-map {'v (make-identity-eval-fn 1)} env)))))
+    ;quoted
+    (is (= 'x (get-result-return (do-eval '(quote x) env))))
+    ;assignment
+    (let [e1 (extend-environment-with-map '{} env)]
+      (let [e2 (get-result-env (do-eval '(set! x 2) e1))]
+        (is (= 2 (get-result-return (do-eval 'x e2))))
+        (let [e3 (get-result-env (do-eval '(set! y (+ 1 2)) e2))]
+          (is (= 3 (get-result-return (do-eval 'y e3))))
+          (let [e4 (get-result-env (do-eval '(set! z (+ x y)) e3))];if x is used instead of z => stack over flow!
+            (is (= 5 (get-result-return (do-eval 'z e4))))
+            ))))
+    ;definition
+    (let [e1 (extend-environment-with-map '{} env)]
+      (let [e2 (get-result-env (do-eval '(define x 2) e1))]
+        (is (= 2 (get-result-return (do-eval 'x e2))))
+        (let [e3 (get-result-env (do-eval '(define y (+ 1 x)) e2))]
+          (is (= 3 (get-result-return (do-eval 'y e3))))
+          (let [e4 (get-result-env (do-eval '(define z (+ x y)) e3))];same here, x causes stack overflow!
+            (is (= 5 (get-result-return (do-eval 'z e4))))
+            (let [e5 (get-result-env (do-eval '(define doubler (lambda (x) (+ x x))) e4))]
+              (is (= 6 (get-result-return (do-eval '(doubler 3) e5))))
+              )))))
+    ;definition of function
+    (let [e1 (extend-environment-with-map '{} env)]
+      (let [e2 (get-result-env (do-eval '(define (doubler x) (+ x x)) e1))]
+        (is (not (nil? (lookup-variable-value-in-env 'doubler e2))))
+        (let [e3 (get-result-env (do-eval '(define y (doubler 3)) e2))]
+          (is (= 6 (get-result-return (do-eval 'y e3))))
+          )))
+    ;if
+    (is (= 1 (get-result-return (do-eval '(if true 1 2) env))))
+    (is (= 2 (get-result-return (do-eval '(if false 1 2) env))))
+    (is (= 'a (get-result-return (do-eval '(if (> 2 1) 'a 'b) env))))
+    (is (= 3 (get-result-return (do-eval '(if (> 2 1) (+ 1 2) 'b) env))))
+    (is (= 5 (get-result-return (do-eval '(if (< 2 1) (+ 1 2) (- 7 2)) env))))
+    ;lambda
+    (is (= 5 (get-result-return (do-eval '((lambda (a b) (+ a b)) 2 3) env))))
+    ;begin
+    (let [e1 (get-result-env (do-eval '(begin (define x 2) (set! y (+ x 1))) env))]
+      (is (= 2 (get-result-return (do-eval 'x e1))))
+      (is (= 3 (get-result-return (do-eval 'y e1))))
+      )
+    ;cond
+    (is (= 1 (get-result-return (do-eval '(cond (true 1) (true 2) (else 3)) env))))
+    (is (= 2 (get-result-return (do-eval '(cond (false 1) (true 2) (else 3)) env))))
+    (is (= 3 (get-result-return (do-eval '(cond (false 1) (false 2) (else 3)) env))))
+    (is (= 3 (get-result-return (do-eval '(cond (false 1) (false 2) (true 3) (else 4)) env))))
+    (is (= 2 (get-result-return (do-eval '(cond ((= 1 (+ 1 1)) 1) ((= 2 (+ 1 1)) 2) (else 3)) env))))
+    (is (= 3 (get-result-return (do-eval '(cond (false 1) (false 2) (else (+ 1 2))) env))))
+    ;application
+    (is (= 2 (get-result-return (do-eval '(* 1 2) env))))
+    (is (= 3 (get-result-return (do-eval '((lambda (a b) (+ a b)) 1 2) env))))
+    (is (= 5 (get-result-return (do-eval '((lambda (a b) (+ a b)) (+ 1 2) 2) env))))
+    (is (= 10 (get-result-return (do-eval '((lambda (a b) (+ a b)) (+ 1 2) ((lambda (a b) (+ a b)) 3 4)) env))))
+    ;recursion
+    (let [e1 (get-result-env (do-eval '(define fact (lambda (n x) (if (= n 1) x (fact (- n 1) (* n x))))) env))]
+      (is (= 1 (get-result-return (do-eval '(fact 1 1) e1))))
+      (is (= 2 (get-result-return (do-eval '(fact 2 1) e1))))
+      (is (= 6 (get-result-return (do-eval '(fact 3 1) e1))))
+      (is (= 24 (get-result-return (do-eval '(fact 4 1) e1))))
+      (is (= (factorial 20) (get-result-return (do-eval '(fact 20 1) e1))))
+      )
+    (let [e1 (get-result-env 
+               (do-eval 
+                 '(define arithmetic-s (lambda (n sum) (if (= n 0) sum (arithmetic-s (- n 1) (+ n sum))))) 
+                 env))]
+      (is (= 1 (get-result-return (do-eval '(arithmetic-s 1 0) e1))))
+      (is (= 3 (get-result-return (do-eval '(arithmetic-s 2 0) e1))))
+      (is (= 6 (get-result-return (do-eval '(arithmetic-s 3 0) e1))))
+      (is (= 190 (get-result-return (do-eval '(arithmetic-s 19 0) e1))))
+      (let [n 100
+            sum (* (/ (+ n 1) 2) n)
+            e2 (get-result-env (do-eval (list 'define 'n n) e1))]
+        (is (= sum (get-result-return (do-eval '(arithmetic-s n 0) e2)))))
+      )
+    )
+  )
+
+;(define fib (lambda (n) (cond ((= n 0) 0) ((= n 1) 1) (else (+ (fib (- n 1)) (fib (- n 2)))))))
+
+(run-tests)
+;(run-all-tests) 
+;(with-junit-output (run-tests))
+;(run-tests)
+;C:\Users\Piotr\workspace\lisp\classes>java -classpath .\..\clojure.jar;C:\Users\Piotr\workspace\lisp\classes clojure.main -i "scheme\interpreter\environmental\s.clj" -e "(use 'clojure.test) (run-all-tests)"
